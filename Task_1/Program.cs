cab763acd31c4444dd60fd00655697f42cf6e43c
Denis <denis-k0504@mail.ru>
2024-07-21 01:23:53 +0300

    Add completed tasks

---
+++ Task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+    Задача 1: Задайте значения M и N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от M до N. 
+Использовать рекурсию, не использовать циклы.
+*/
+
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+ViewRangeIntegers(m, n);
+
+void ViewRangeIntegers(int m, int n) {
+    if (m > n) return;
+
+    Console.Write($"{m}" + $"{(m != n ? ", " : " ")}");
+
+    ViewRangeIntegers(m + 1, n);
+}
---
+++ Task_3/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+    Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+    Использовать рекурсию, не использовать циклы.
+*/
+
+Console.WriteLine("Исходный массив:");
+int[] resultOriginalArray = GetOriginalArray();
+Console.WriteLine("");
+
+RevertArray(resultOriginalArray);
+
+void RevertArray(int[] array) {
+    if (array.Length < 1) return;
+
+    Console.Write($"{array[array.Length - 1]} ");
+
+    RevertArray(array[..(array.Length - 1)]);
+}
+
+int[] GetOriginalArray() {
+    int[] originalArray = new int[5];
+    Random randNum = new Random();
+
+    for (int i = 0; i < originalArray.Length; i++)
+    {
+        int val = randNum.Next(0, 20);
+        originalArray[i] = val;
+
+        Console.Write($"{val} ");
+    }
+
+    return originalArray;
+}
+
